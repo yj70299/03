0542b4eced1a19a6f799f8b0e0c1bdba6fdcd146
yj7029 <92070320+yj7029@users.noreply.github.com>
2021-10-30 00:59:39 +0900

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,41 @@
+let numFrames = 11;
+let images = [];
+let currentFrame = 0;
+
+function preload() {
+  // eyeClosed = loadImage("EYES/EYE_010.PNG");
+  for (let i = 0; i < numFrames; i++) {
+    images[i] = loadImage("EYES/EYE_" + nf(i,3) + ".PNG");
+  }
+
+}
+
+function setup() {
+  createCanvas(800, 400);
+  frameRate(12);
+}
+
+function draw() {
+  clear();
+  image(images[currentFrame], 0, 0, 800, 400);
+
+  if (currentFrame == images.length - 1) {
+    currentFrame = 0;
+  }
+  currentFrame++;
+
+  fill(0);
+  ellipse(50, 50, 50, 70);
+  textSize(20);
+  text("↑ 긴장을 주고 싶다면 ", 40, 110);
+
+  if (mouseX < 100 && mouseY < 120){
+    if(mouseIsPressed){  
+      frameRate(40);
+    }
+  } else {
+    frameRate(12);
+  }
+
+
+}
